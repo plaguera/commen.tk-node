7d2b2bdd9cf00158ab1a9d48a44b781f78c10c97
Pedro Lagüera <laguerapedro@gmail.com>
2020-10-04 00:03:29 +0200

    Removed https express

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,12 @@
 require('dotenv').config();
-import express from './server'
-import fs from 'fs';
-import https from 'https';
+import express from './server';
 import log from './logger';
 import env from './environment';
 
 log.debug('NODE_ENV', env.node_env);
 log.debug('CWD\t', env.cwd);
-let port = env.port || '8000';
+let port = env.port || '8080';
 
-switch (env.node_env) {
-    case 'DEVELOPMENT': httpServer(); break;
-    case 'PRODUCTION': httpsServer(); break;
-}
-
-/**
- * Creates a HTTPS server using a domain private key and  certificate on port @port
- * Used in production.
- */
-function httpsServer() {
-	https.createServer({
-		key: fs.readFileSync('/etc/letsencrypt/live/api.commen.tk/privkey.pem'),
-		cert: fs.readFileSync('/etc/letsencrypt/live/api.commen.tk/cert.pem'),
-	}, express).listen(port, () => {
-		console.log(`Server started on port ${port}`);
-	});
-}
-
-/**
- * Creates a HTTP server on port @port
- * Used in only in development and testing environments.
- */
-function httpServer() {
-	express.listen(port, () => {
-		console.log(`Server started on port ${port}`);
-	});
-}
+express.listen(port, () => {
+	console.log(`Server started on port ${port}`);
+});
